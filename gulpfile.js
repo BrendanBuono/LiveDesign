fa00eb6f51874582ab40d97886a7632179ad1274
Brendan Buono <brendan.buono@gmail.com>
2015-05-20 07:33:07 -0400

    Updated build
    Build updated to copy html and css file, still needs concatentation of
    build tasks

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
+
+// add custom browserify options here
+var customOpts = {
+  entries: ['./app/js/main.js'],
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+// add transformations here
+// i.e. b.transform(coffeeify);
+
+gulp.task('css',function(){
+ return gulp.src('app/css/*.css')
+ .pipe(gulp.dest('./build/css'));
+});
+gulp.task('copy-index',function(){
+ return gulp.src('app/index.html')
+ .pipe(gulp.dest('./build'));
+});
+
+gulp.task('js',bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./build/js'));
+}
